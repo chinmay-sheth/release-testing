864244640573dfbeb3737ec24dcfcc70b770b190
chinmay-sheth <66354481+chinmay-sheth@users.noreply.github.com>
2026-01-05 17:03:40 +0530

    Multiplication function

--- src/index.js
+++ src/index.js
@@ -12,3 +12,18 @@ module.exports = add;
 
 // Example:
 // console.log(add(2, 3)); // 5
+
+/**
+ * Multiply two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function multiply(a, b) {
+  return Number(a) * Number(b);
+}
+
+module.exports = { add: module.exports, multiply };
+
+// Example:
+// console.log(multiply(2, 3)); // 6
