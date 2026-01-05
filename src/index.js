885af5654a1c8fa1dd957de10f3e4d49d1440723
chinmay-sheth <66354481+chinmay-sheth@users.noreply.github.com>
2026-01-05 16:59:50 +0530

    Pilot

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/**
+ * Add two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+
+module.exports = add;
+
+// Example:
+// console.log(add(2, 3)); // 5
